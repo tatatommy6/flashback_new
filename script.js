acf071a7b1e64f70125205522898e7131b401f2c
rrayy <rrayy25809@gmail.com>
2024-09-21 13:12:41 +0900

    Changes to be committed: 페이지 3 제작, JS 시작 	modified:   input.html 	new file:   script.js 	modified:   source.css

---
+++ script.js
@@ -0,0 +1,19 @@
+const awfeawe = `<img class="output-image" src="/img/test_image.png"/>
+        <div class="storyboard">
+          <h5>Explain Storyboard</h5>
+          <textarea placeholder="Describe the storyboard here"></textarea>
+          <div class="buttons">
+            <button class="button-vr" onclick="">Make VR</button>
+            <button class="button-next" onclick="">Next</button>
+          </div>
+        </div>
+      </div>`
+
+const awsfwefawefa = ``
+
+function generate(){
+    var image_input = document.getElementById("image-input").files;
+    var prompt_input = document.getElementById("prompt-input").value;
+    var output = document.getElementById("right");
+    output.innerHTML = awfeawe;
+}
